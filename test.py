e44e09cf643b2f32f6ae1e1d75692f0b753110ed
jessicaloohw <32312766+jessicaloohw@users.noreply.github.com>
2020-10-30 10:54:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def main():
 
     ################################### USER INPUT #####################################################
 
-    MODEL_DIR = './Trained_Networks'
+    MODEL_DIR = './models'
     IMAGE_FILENAME = './path/to/image/filename.tif'
     CAV_SEGMENTER_NUMBER = 1    # 1, 2
     K_FOLD_NUMBER = 2           # 1, 2, 3, 4, 5, 6, 7, 8, 9
